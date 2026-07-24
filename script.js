6a3e15c9e72085e4986849d31a215c0180aa4233
Thaneshwar <durgachoicecenter99@gmail.com>
2026-07-24 16:00:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+// =======================
+// Automatic Slider
+// =======================
+
+const slides = [
+    "images/banner1.jfif",
+    "images/banner2.jfif",
+    "images/banner3.jfif",
+    "images/banner4.jfif"
+];
+
+let current = 0;
+
+function autoSlider() {
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    document.getElementById("slide").src = slides[current];
+
+}
+
+setInterval(autoSlider,3000);
+
+
+// =======================
+// Live Search
+// =======================
+
+function searchService(){
+
+    let input = document.getElementById("search").value.toUpperCase();
+
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card){
+
+        let name = card.getAttribute("data-name").toUpperCase();
+
+        if(name.indexOf(input)>-1){
+
+            card.style.display="block";
+
+        }else{
+
+            card.style.display="none";
+
+        }
+
+    });
+
+}
+
+
+// =======================
+// Card Click
+// =======================
+
+const serviceLinks={
+
+"CG Shram Card":"https://shramevjayate.cg.gov.in/shramik",
+
+"PAN Card Service":"https://www.onlineservices.nsdl.com/",
+
+"Aadhaar Service":"https://myaadhaar.uidai.gov.in/",
+
+"APAAR ID":"https://apaar.education.gov.in/",
+
+"Voter ID":"https://voters.eci.gov.in/",
+
+"CG Ration Card":"https://khadya.cg.nic.in/",
+
+"Ayushman Bharat":"https://beneficiary.nha.gov.in/",
+
+"CG Rojgar Panjiyan":"https://exchange.cg.nic.in/",
+"Driving Licence":"https://parivahan.gov.in/",
+"RC Service":"https://parivahan.gov.in/",
+"Vehicle Service":"https://parivahan.gov.in/",
+"HSRP":"https://cgtransport.gov.in/#",
+"आय/जाति/निवास":"https://sewasetu.cgstate.gov.in/home?lang=hi",
+"Birth&Death":"https://dc.crsorgi.gov.in/crs/Auth/general-public",
+"EPFO Service":"https://unifiedportal-emp.epfindia.gov.in/epfo/",
+"Udyam Aadhar":"https://udyamregistration.gov.in/UdyamRegistration.aspx",
+"PM Kisan Samman Nidhi":"https://pmkisan.gov.in/",
+"PM Fasal Bima Yojana":"https://pmfby.gov.in/",
+"PM Awas Yojana":"https://pmayg.dord.gov.in/netiayHome/Home.aspx",
+"E-sharam card":"https://eshram.gov.in/",
+"Hemchand yadav":"https://www.durguniversity.ac.in/",
+"Durg Science Clg":"https://www.govtsciencecollegedurg.ac.in/",
+"CGBSE":"https://cgbse.nic.in/"
+
+};
+
+
+document.querySelectorAll(".card").forEach(function(card){
+
+card.addEventListener("click",function(){
+
+let service=this.getAttribute("data-name");
+
+if(serviceLinks[service]){
+
+window.open(serviceLinks[service],"_blank");
+
+}
+
+});
+
+});
+
+
+// =======================
+// Hover Animation
+// =======================
+
+document.querySelectorAll(".card").forEach(function(card){
+
+card.addEventListener("mouseenter",function(){
+
+this.style.transform="translateY(-8px) scale(1.02)";
+
+});
+
+card.addEventListener("mouseleave",function(){
+
+this.style.transform="translateY(0px)";
+
+});
+
+});
